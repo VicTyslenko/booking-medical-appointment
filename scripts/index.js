2ca6fa0f97a941319af6c0860eda54347c74780c
Oleh Verbynskyi <trewa10@gmail.com>
2022-09-12 16:48:14 +0300

    new visit form design is done, still working on validation and functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,62 @@
 import {getToken, sendCard, deleteCard, getCards, getCard, editCard} from './functions/send-request.js';
-import {Modal, ModalLogin} from './classes/modal.js';
+import {Modal, ModalLogin, ModalAddCard} from './classes/modal.js';
 
 
 // тут будуть глобальні змінні
 const API = 'https://ajax.test-danit.com/api/v2/cards';
+let visitsCollection = [];                                  // масив усіх візитів
+
 
 
 // змінні щоб їх було видно для всіх функцій нижче Типу глобальні змінні, але ще в процесі тестування і можливо їх не буде в фінальному білді
 let entryModal; // обєкт з вікном входу
 let keyToken; // сюди записується токен  Наступні функції запиту на сервер (для отримання карток чи ін) слід викликати з перевіркою if(keyToken)
+let newVisitModal; // обєкт з вікном створення нового візиту
 
 
+// Тест елементів форми
+// // шукаємо форму
+// const form = document.querySelector('#newVisitForm');
+// // шукаємо всі приховані блоки
+// const allDoctorsBlock = form.querySelector('#forAllDoctors');
+// const cardiologistBlock = form.querySelector('#forCardiologist');
+// const dentistBlock = form.querySelector('#forDentist');
+// const therapistBlock = form.querySelector('#forTherapist');
+// const saveBtn = form.querySelector('#create-btn');
+// // навішуємо обробник на форму і по вибору лікаря відкриваємо потрібні поля
+// form.addEventListener('change', (e) => {
+//     if(e.target === form.querySelector('#selectDoctor')) {
+//         console.log(e.target.value);
+//         if(e.target.value === 'cardiologist') {
+//             allDoctorsBlock.classList.remove('hidden');
+//             cardiologistBlock.classList.remove('hidden');
+//             saveBtn.classList.remove('hidden');
+//             dentistBlock.classList.add('hidden');
+//             therapistBlock.classList.add('hidden');
+//         } else if(e.target.value === 'dentist') {
+//             allDoctorsBlock.classList.remove('hidden');
+//             dentistBlock.classList.remove('hidden');
+//             saveBtn.classList.remove('hidden');
+//             cardiologistBlock.classList.add('hidden');
+//             therapistBlock.classList.add('hidden');
+//         } else if(e.target.value === 'therapist') {
+//             allDoctorsBlock.classList.remove('hidden');
+//             therapistBlock.classList.remove('hidden');
+//             saveBtn.classList.remove('hidden');
+//             cardiologistBlock.classList.add('hidden');
+//             dentistBlock.classList.add('hidden');
+//         }
+//     }
+// })
 
+// form.addEventListener('submit', function (event) { 
+//     if (!form.checkValidity()) { 
+//       event.preventDefault()
+//       event.stopPropagation()
+//     }
+
+//     form.classList.add('was-validated')
+//   }, false)
 
 
 
@@ -21,7 +66,6 @@ document.addEventListener('click', async (e) => {
     if (e.target.id === 'entry-btn') {              // якщо нажати кнопку входу
         entryModal = new ModalLogin();
         entryModal.render();
-        console.log(entryModal);
     } else if (e.target.id === 'login-btn') {       //якщо нажати кнопку на формі відправки логіна та пароля
         e.preventDefault();
         let login = document.querySelector('#inputEmail').value;
@@ -41,12 +85,51 @@ document.addEventListener('click', async (e) => {
             console.log('Wrong values!');
         }
         if(keyToken) {
+            // міняємо кнопки
             document.querySelector('#entry-btn').classList.add('invisible');
             document.querySelector('#visit-btn').classList.remove('hidden');
+
+            // показуємо форму пошуку
+            document.querySelector('#sorting-form').classList.remove('hidden');
+
+            // тут також має бути функція отримання всіх карток
+            await getCards(API, keyToken).then(cardsList => visitsCollection = cardsList);
+            // і функція рендеру всіх наявних карток, яка приймає масив усіх карток і створює по класу нові картки і виводить їх на екран
+            // щось типу такого visitsRender(visitsCollection)
         }
 
         
-        // тут потім треба додати заміну кнопки входу на кнопку створення нової каркти
+    } else if (e.target.id === 'visit-btn') {                // якщо натиснути кнопку виклику вікна створення нового візиту
+        newVisitModal = new ModalAddCard();
+        newVisitModal.render();
+    } else if (e.target.id === 'create-btn') {                 // якщо натиснути кнопку створити новий візит
+        e.preventDefault();
+        
+        // валідація форми по документації bootstrap
+        const form = document.querySelector('#newVisitForm');
+        if (!form.checkValidity()) {  // не впевнений що цей блок працює як треба
+            e.preventDefault()
+            e.stopPropagation()
+            
+        } 
+        form.classList.add('was-validated')
+
+        
+            // блок ще в роботі)
+            // тут буде функція створення нової картки і відправка на сервер
+            console.log(form);
+
+        
+
+
+
+
+
+
+
+
+
+
     }
 
 
