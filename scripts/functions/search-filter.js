7e03f1c4319bf7080d8bb2ffd737d1a5a8b8e2a4
Victor Tyslenko <vidrums86@gmail.com>
2022-09-27 16:13:07 +0300

    last_fix

--- scripts/functions/search-filter.js
+++ scripts/functions/search-filter.js
@@ -21,7 +21,7 @@ if(sortingForm.urgency.value === 'high'){
                    const visit = new VisitTherapist(card)
                    visit.render(cardsWrapper)
                });
-}
+} 
 else if(sortingForm.urgency.value === 'middle'){
     let filteredArray = array.filter(card => card.doctor === 'Therapist' && card.urgency === 'middle')
                cardsWrapper.innerHTML = '';
@@ -97,14 +97,11 @@ else if(sortingForm.title.value === 'Cardiologist' || sortingForm.title.value ==
         }
 }
 else if(sortingForm.status.value === 'done'){
-// alert('done')
 cardsWrapper.innerHTML =''
 cardsWrapper.insertAdjacentHTML('afterbegin','<h1 class="done-text"> All visits are done</h1>')
 }
 
-// console.log(sortingForm.title.value);
-// console.log(sortingForm.urgency.value);
-// console.log(sortingForm.status.value);
+
 }
   
 
@@ -124,139 +121,6 @@ export default function searchFilter(array){
         })
     
     };
-    // sortingForm.addEventListener('change',function(event){
-        // console.log(this,event);
-        // let target = event.target;
-       
-        // if(target === status){
-         
-        // if(status.value === 'open'){
-
-        // }
-        
-        // };
-        // if(target === inputTitle){
-
-        //     if(inputTitle.value === 'Cardiologist'){
-        //         if(urgency.value === 'high'){
-        //             let filteredArray = array.filter(card=>card.doctor === 'Cardiologist' && card.urgency === 'high')
-        //             console.log(filteredArray);
-        //             cardsWrapper.innerHTML = '';
-        //          renderCards(filteredArray)
-        //         }
-        //     }
-
-        // };
-        // if(target === urgency){
-         
-        // }
-    // })
-
-
-
-
-
-
-
-
-
-
-//    sortingForm.addEventListener('',(event)=>{
-
-//     if(event.target.value === 'open'){
-        
-//         // Кардiолог  фiльтр
-//      if(inputTitle.value === 'Cardiologist'){
-//        if(urgency.value == 'high'){
-//            let filteredArray = array.filter(card => card.doctor === 'Cardiologist' && card.urgency === 'high')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitCardiologist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'middle'){
-//            let filteredArray = array.filter(card => card.doctor === 'Cardiologist' && card.urgency === 'middle')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitCardiologist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'low'){
-//            let filteredArray = array.filter(card => card.doctor === 'Cardiologist' && card.urgency === 'low')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitCardiologist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//      }
-//    //   Терапевт фiльтр
-//      if(inputTitle.value === 'Therapist'){
-//        if(urgency.value === 'high'){
-//            let filteredArray = array.filter(card => card.doctor === 'Therapist' && card.urgency === 'high')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitTherapist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'middle'){
-//            let filteredArray = array.filter(card => card.doctor === 'Therapist' && card.urgency === 'middle')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitTherapist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'low'){
-//            let filteredArray = array.filter(card => card.doctor === 'Therapist' && card.urgency === 'low')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitTherapist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//      }
-   
-//    //   Дантист фiльтр
-//      if(inputTitle.value === 'Dentist'){
-//        if(urgency.value == 'high'){
-//            let filteredArray = array.filter(card => card.doctor === 'Dentist' && card.urgency === 'high')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitDentist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'middle'){
-//            let filteredArray = array.filter(card => card.doctor === 'Dentist' && card.urgency === 'middle')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitDentist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//        else if(urgency.value === 'low'){
-//            let filteredArray = array.filter(card => card.doctor === 'Dentist' && card.urgency === 'low')
-//            cardsWrapper.innerHTML = '';
-//            filteredArray.forEach(card => {
-//                const visit = new VisitDentist(card)
-//                visit.render(cardsWrapper)
-//            });
-//        }
-//      }
-   
-        //  }
-    //      else{
-             
-    //        cardsWrapper.innerHTML = '<p class="visits-done">all visits are done</p>'
-   
-    //    }
-
-//    })
-
    
 
 
